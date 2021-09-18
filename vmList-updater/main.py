f623a094196dc93356b0b98ee5be5af609fb4a6b
walt4771 <42925880+walt4771@users.noreply.github.com>
2021-09-18 17:30:19 +0900

    Add files via upload

--- vmList-updater/main.py
+++ vmList-updater/main.py
@@ -1,19 +1,49 @@
-# UI info
-# json 파일 위치
-# esx 로그인 정보, ip (저장또는 일회용)
-
-# Logic
-# Info o
-# json 새로고침 o
-# VMName, VMID o
-# On/Off 정보 o
-# 새로고침 웹상에서 대기 x
-
 import paramiko
 import time
 import json
 
+import sys
 
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtWidgets import *
+from PyQt5 import uic, QtCore, QtWidgets
+import res_image
+
+esx_id = ""
+esx_pw = ""
+esx_ip = ""
+saveinfo = False
+
+# UI
+form_class = uic.loadUiType("esxLoginForm.ui")[0]
+
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        window = QPalette()
+        window.setColor(QPalette.Background, QColor(20, 20, 40))
+        self.setAutoFillBackground(True)
+        self.setPalette(window)
+
+        self.setupUi(self)
+
+    def selectJSONFile(self):
+        vmInfo = QtWidgets.QFileDialog.getOpenFileName(self, 'open file')
+        vmInfo = vmInfo[0]
+
+    def submitLogin(self):
+        saveinfo = self.checkbox_saveinfo.isChecked()
+        esx_ip = self.textedit_esx_ip.toPlainText()
+        esx_id = self.textedit_esx_id.toPlainText()
+        esx_pw = self.lineedit_esx_pw.text()
+        print(esx_pw + esx_id + esx_ip)
+        QMessageBox.about(self, "message", "login")
+        QtCore.QCoreApplication.instance().quit()
+
+
+# Login/SSH Logic
 def getLoginInfo():
     with open('LoginInfo.json') as f:
         data = json.load(f)
@@ -29,42 +59,57 @@ def getVmList(jsonLocation):
 def connect_SSH(ip, id, pw, cmd):
     cli = paramiko.SSHClient()
     cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    cli.connect(ip, username=id, password=pw)
-    # 새로운 interactive shell session 생성
+    print(type(ip))
+    cli.connect(hostname=ip, username=id, password=pw)
+    # 세션
     channel = cli.invoke_shell()
-    # 명령 송신
+    print("connected")
+    # ->
     channel.send(cmd + "\n")
     time.sleep(1.0)
-    # 결과 수신
+    # <-
     output = channel.recv(65535).decode("utf-8")
     cli.close()
     return output
 
 
+def getVMState(id, ip, pw):
+    connect_ssh_VMID = str(loginInfo.get('VMID'))
+    print("vmstate")
+    powerState = [0 for i in range(5)]
+    for i in range(0, 5):
+        cmd = vmInfo[i].get("VMID")
+        powerState[i] = connect_SSH(
+            ip,
+            id,
+            pw,
+            "vim-cmd vmsvc/power.getstate " + cmd
+        )
+        if powerState[i].find('Powered on') == -1:
+            powerState[i] = "Powered off"
+        else:
+            powerState[i] = "Powered on"
+        vmInfo[i]['isPoweredOn'] = powerState[i]
+
+        with open(loginInfo.get("jsonFile"), 'w', encoding='utf-8') as f:
+            json.dump(vmInfo, f, indent="\t")
+
+
+# Exec
 # Init
 loginInfo = getLoginInfo()
 vmInfo = getVmList(loginInfo.get("jsonFile"))
 
-# SSH
-connect_SSH_ip = str(loginInfo.get('ip'))
-connect_SSH_id = str(loginInfo.get('id'))
-connect_SSH_pw = str(loginInfo.get('pw'))
-connect_SSH_VMID = str(loginInfo.get('VMID'))
+print(loginInfo.get('isLoginInfoSaved'))
+if loginInfo.get('isLoginInfoSaved') == "yes":
+    getVMState(esx_ip, esx_id, esx_pw)
+else:
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
 
-powerState = [0 for i in range(5)]
-for i in range(0, 5):
-    cmd = vmInfo[i].get("VMID")
-    powerState[i] = connect_SSH(
-        connect_SSH_ip,
-        connect_SSH_id,
-        connect_SSH_pw,
-        "vim-cmd vmsvc/power.getstate " + cmd
-    )
-    if powerState[i].find('Powered on') == -1:
-        powerState[i] = "Powered off"
-    else:
-        powerState[i] = "Powered on"
-    vmInfo[i]['isPoweredOn'] = powerState[i]
-
+    loginInfo["isLoginInfoSaved"] = 'yes'
     with open(loginInfo.get("jsonFile"), 'w', encoding='utf-8') as f:
         json.dump(vmInfo, f, indent="\t")
+    getVMState(esx_ip, esx_id, esx_pw)
