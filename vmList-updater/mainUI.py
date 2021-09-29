fa1f80d7717d593ae908c45374b889749cd27a0a
walt4771 <42925880+walt4771@users.noreply.github.com>
2021-09-29 22:33:49 +0900

    v.210929
    - add window drag
    - add close button on window

--- vmList-updater/mainUI.py
+++ vmList-updater/mainUI.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtGui import QPalette, QColor, QCursor
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtCore, QtWidgets
 import res_image
@@ -27,3 +27,23 @@ class MyWindow(QMainWindow, form_class):
     def submitLogin(self):
         QtCore.QCoreApplication.instance().quit()
         return self.textedit_esx_ip.toPlainText(), self.textedit_esx_id.toPlainText(), self.lineedit_esx_pw.text(), self.checkbox_saveinfo.isChecked(), vmInfo
+
+    def exitUI(self):
+        QtCore.QCoreApplication.instance().quit()
+        return 0
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.m_flag = True
+            self.m_Position = event.globalPos() - self.pos()  # Get the position of the mouse relative to the window
+            event.accept()
+            self.setCursor(QCursor(QtCore.Qt.OpenHandCursor))  # Change mouse icon
+
+    def mouseMoveEvent(self, QMouseEvent):
+        if QtCore.Qt.LeftButton and self.m_flag:
+            self.move(QMouseEvent.globalPos() - self.m_Position)  # Change window position
+            QMouseEvent.accept()
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_flag = False
+        self.setCursor(QCursor(QtCore.Qt.ArrowCursor))
